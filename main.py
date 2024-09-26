7bdb3f8b808755f897137bb6e52ce7b655ca0a44
eevanoraa <arturs.eglajs@gmail.com>
2024-09-26 12:36:49 +0300

    d

---
+++ main.py
@@ -0,0 +1,8 @@
+import math
+
+a = float(input("Cik cm ir mala a?: "))
+b = float(input("Cik cm ir mala b?: "))
+
+result = a * b
+
+print(f"Tavs rezultāts ir {round(result)} cm².")
---
+++ mainv2.py
@@ -0,0 +1,8 @@
+saule = input("Vai spīd saule? (Jā/Nē): ")
+
+if saule == "Jā":
+    print("Ārā ir sauļains.")
+elif saule == "Nē":
+    print("Ārā nav sauļains.")
+else:
+    print(f"Ievade {saule} nav pareiza.")
