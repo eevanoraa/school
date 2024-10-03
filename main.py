765dd29820fbb4d97fe560b623f2fab5dd14db61
eevanoraa <arturs.eglajs@gmail.com>
2024-10-03 12:51:23 +0300

    main

---
+++ main.py
@@ -0,0 +1,7 @@
+# python ievadstunda
+
+name = input("Ievadi savu vārdu: ")
+age = int(input("Ievadi savu vecumu: "))
+age_next_year = age + 1
+
+print(f"Tavs vārds ir {name} un nākamgad tev būs {age_next_year} gadi!")
