08748555eb9c1891c55163bceb6cdd954433f944
evanora <sokarihinji@gmail.com>
2024-12-19 23:41:16 +0200

    Update dzeramnauda.py

--- dzeramnauda.py
+++ dzeramnauda.py
@@ -1,37 +1,28 @@
 def main():
-    eur = eur_to_float(input("Cik maksāja pusdienas? "))
-    valuta = ""
-    eur = valuta
+    eur, valuta = eur_to_float(input("Cik maksāja pusdienas? "))
     procenti = procenti_to_float(input("Cik procentus maksāsi viesmīlim dzeramnaudā? "))
     dzeramnauda = eur * procenti
-    print(f"Jāatstāj {valutas} {dzeramnauda:.2f}")
+    print(f"Jāatstāj {dzeramnauda:.2f} {valuta}")
 
 def eur_to_float(eur_value):
+    number = None
+    currency = None
     for word in eur_value.split():
         try:
-            number = (float(word))
+            number = float(word)
         except ValueError:
-            pass
-    
-    return number
-
-def valutas(teikums):
-    for word in teikums.split():
-        try:
-            pass
-        except ValueError:
-            valuta = 
-
-# valutas funkcija atrast no teikuma to valutu ko ievada.
-
+            currency = word
+    return number, currency
 
 def procenti_to_float(procent_value):
+    percentage = None
     for word in procent_value.split():
         try:
-            percentage = (float(word))
+            percentage = float(word)
         except ValueError:
             pass
     return percentage / 100
-    
 
-main()
+
+if __name__ == "__main__":
+    main()
