862a5c9629d18bff34306f1b6a7871b40c72e3af
evanora <sokarihinji@gmail.com>
2024-12-19 14:36:24 +0200

    yeah

--- license plate.py
+++ license plate.py
@@ -1,17 +1,17 @@
 import re
 
 plate = str(input("Ievadi numuru zimi (AB-1234): "))
-plate = plate.upper()
+plate = plate.upper().replace("-", "")
 
     # Regex izskaidrojums:
     # ^                 - String sākums
     # [A-PR-VZ]{2}      - Tieši divi burti (izņemot Q, W, Y)
-    # -                 - Svītra starp burtiem un skaitļiem
     # [1-9]\d{0,3}      - 1-4 cipari, sākot ar 1-9 tā, lai numuru zīmes nevarētu sākties ar 0
     # $                 - String beigas
-pattern = r'^[A-PR-VZ]{2}-[1-9]\d{0,3}$'
+pattern = r'^[A-PR-VZ]{2}[1-9]\d{0,3}$'
 
 test = (bool(re.match(pattern, plate)))
+
 if test == True:
     print("Numurs ir derīgs")
 else:
